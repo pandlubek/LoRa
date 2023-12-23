566bc773eec3ee3c470dbb47bcd172442ca28899
Pan Dłubek <pandlubek@onet.pl>
2023-12-23 17:21:47 +0100

    LoRa radio parameters have been adjusted to achieve a longer transmission distance

--- Heltec_LoRa32_DoorSensor/files/configuration.h
+++ Heltec_LoRa32_DoorSensor/files/configuration.h
@@ -12,9 +12,9 @@
 
 // LoRa
 #define RF_FREQUENCY                868000000 // Hz
-#define TX_OUTPUT_POWER             5 // dBm
+#define TX_OUTPUT_POWER             21 // dBm
 #define LORA_BANDWIDTH              0 // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
-#define LORA_SPREADING_FACTOR       7 // [SF7..SF12]
+#define LORA_SPREADING_FACTOR       9 // [SF7..SF12]
 #define LORA_CODINGRATE             1 // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
 #define LORA_PREAMBLE_LENGTH        8 // Same for Tx and Rx
 #define LORA_SYMBOL_TIMEOUT         0 // Symbols
--- Heltec_LoRa32_MQTTGateway/files/configuration.h
+++ Heltec_LoRa32_MQTTGateway/files/configuration.h
@@ -10,7 +10,7 @@
 #define RF_FREQUENCY                868000000 // Hz
 #define TX_OUTPUT_POWER             14 // dBm
 #define LORA_BANDWIDTH              0 // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
-#define LORA_SPREADING_FACTOR       7 // [SF7..SF12]
+#define LORA_SPREADING_FACTOR       9 // [SF7..SF12]
 #define LORA_CODINGRATE             1 // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
 #define LORA_PREAMBLE_LENGTH        8 // Same for Tx and Rx
 #define LORA_SYMBOL_TIMEOUT         0 // Symbols
